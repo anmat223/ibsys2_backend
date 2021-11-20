979a31bfb236b547dffbd6afa966347120f4ddf0
Vincent Mielke <Voelvimi@aol.com>
2021-11-20 13:59:57 +0100

    Verwendung für Fahrräder erweitert

--- classes/entities/Kaufteil.php
+++ classes/entities/Kaufteil.php
@@ -1,8 +1,11 @@
 <?php
 class Kaufteil extends Teil
 {
-    public int $eingehendeBestellungen;
-    public float $lieferzeit;
-    public float $abweichung;
-    public int $diskontmenge;
+  public int $eingehendeBestellungen;
+  public float $lieferzeit;
+  public float $abweichung;
+  public int $diskontmenge;
+  public int $verwendungP1;
+  public int $verwendungP2;
+  public int $verwendungP3;
 }
