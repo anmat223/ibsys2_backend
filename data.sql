16db86c6509172876a6b3ce5c690ad936ee35237
anmat223 <43849962+anmat223@users.noreply.github.com>
2021-11-23 13:51:57 +0100

    Add files via upload
    added file to add data to mysql-database

---
+++ data.sql
@@ -0,0 +1,347 @@
+# Produktionsteile
+
+INSERT INTO teil (nummer, preis)
+	VALUES (1, 156.13);
+INSERT INTO produktionsteil 
+	VALUES (1, 0, 80);
+	
+INSERT INTO teil (nummer, preis)
+	VALUES (2, 163.33);
+INSERT INTO produktionsteil 
+	VALUES (2, 0, 100);
+	
+INSERT INTO teil (nummer, preis)
+	VALUES (3, 165.08);
+INSERT INTO produktionsteil 
+	VALUES (3, 0, 60);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (4, 40.85);
+INSERT INTO produktionsteil 
+	VALUES (4, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (5, 39.85);
+INSERT INTO produktionsteil 
+	VALUES (5, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (6, 40.85);
+INSERT INTO produktionsteil 
+	VALUES (6, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (7, 35.85);
+INSERT INTO produktionsteil 
+	VALUES (7, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (8, 35.85);
+INSERT INTO produktionsteil 
+	VALUES (8, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (9, 35.85);
+INSERT INTO produktionsteil 
+	VALUES (9, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (10, 12.4);
+INSERT INTO produktionsteil 
+	VALUES (10, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (11, 14.65);
+INSERT INTO produktionsteil 
+	VALUES (11, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (12, 14.65);
+INSERT INTO produktionsteil 
+	VALUES (12, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (13, 12.4);
+INSERT INTO produktionsteil 
+	VALUES (13, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (14, 14.65);
+INSERT INTO produktionsteil 
+	VALUES (14, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (15, 14.65);
+INSERT INTO produktionsteil 
+	VALUES (15, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (16, 7.02);
+INSERT INTO produktionsteil 
+	VALUES (16, 1, 150);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (17, 7.16);
+INSERT INTO produktionsteil 
+	VALUES (17, 1, 150);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (18, 13.15);
+INSERT INTO produktionsteil 
+	VALUES (18, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (19, 14.35);
+INSERT INTO produktionsteil 
+	VALUES (19, 0, 100);
+	
+INSERT INTO teil (nummer, preis)
+	VALUES (20, 15.55);
+INSERT INTO produktionsteil 
+	VALUES (20, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (26, 10.5);
+INSERT INTO produktionsteil 
+	VALUES (26, 1, 150);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (29, 69.29);
+INSERT INTO produktionsteil 
+	VALUES (29, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (30, 127.53);
+INSERT INTO produktionsteil 
+	VALUES (30, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (31, 144.42);
+INSERT INTO produktionsteil 
+	VALUES (31, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (49, 64.64);
+INSERT INTO produktionsteil 
+	VALUES (49, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (50, 120.63);
+INSERT INTO produktionsteil 
+	VALUES (50, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (51, 137.47);
+INSERT INTO produktionsteil 
+	VALUES (51, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (54, 68.09);
+INSERT INTO produktionsteil 
+	VALUES (54, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (55, 125.33);
+INSERT INTO produktionsteil 
+	VALUES (55, 0, 100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (56, 142.67);
+INSERT INTO produktionsteil 
+	VALUES (56, 0, 100);
+
+
+
+# Kaufteile
+
+INSERT INTO teil (nummer, preis)
+	VALUES (21, 5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (21, 1.8, 0.4, 300);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (22, 6.5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (22, 1.7, 0.4, 300);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (23, 6.5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (23, 1.2, 0.2, 300);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (24, 0.06);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (24, 3.2, 0.3, 6100);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (25, 0.06);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (25, 0.9, 0.2, 3600);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (27, 0.1);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (27, 0.9, 0.2, 1800);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (28, 1.2);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (28, 1.7, 0.4, 4500);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (32, 0.75);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (32, 2.1, 0.5, 2700);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (33, 22);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (33, 1.9, 0.5, 900);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (34, 0.1);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (34, 1.6, 0.3, 22000);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (35, 1);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (35, 2.2, 0.4, 3600);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (36, 8);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (36, 1.2, 0.1, 900);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (37, 1.5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (37, 1.5, 0.3, 900);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (38, 1.5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (38, 1.7, 0.4, 300);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (39, 1.5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (39, 1.5, 0.3, 1800);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (40, 2.5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (40, 1.7, 0.2, 900);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (41, 0.06);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (41, 0.9, 0.2, 900);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (42, 0.1);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (42, 1.2, 0.3, 1800);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (43, 5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (43, 2, 0.5, 2700);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (44, 0.5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (44, 1, 0.2, 900);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (45, 0.06);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (45, 1.7, 0.3, 900);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (46, 0.1);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (46, 0.9, 0.3, 900);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (47, 3.5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (47, 1.1, 0.1, 900);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (48, 1.5);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (48, 1, 0.2, 1800);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (52, 22);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (52, 1.6, 0.4, 600);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (53, 0.1);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (53, 1.6, 0.2, 22000);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (57, 22);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (57, 1.7, 0.3, 600);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (58, 0-1);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (58, 1.6, 0.5, 22000);
+
+INSERT INTO teil (nummer, preis)
+	VALUES (59, 0.15);
+INSERT INTO kaufteil (teil, lieferzeit, abweichung, diskontmenge)
+	VALUES (59, 0.7, 0.2, 1800);
+
+
+
+# Arbeitsplätze
+
+INSERT INTO arbeitsplatz
+	VALUE (1, 60);
+
+INSERT INTO arbeitsplatz
+	VALUE (2, 80);
+
+INSERT INTO arbeitsplatz
+	VALUE (3, 60);
+
+INSERT INTO arbeitsplatz
+	VALUE (4, 80);
+
+INSERT INTO arbeitsplatz
+	VALUE (6, 60);
+
+INSERT INTO arbeitsplatz
+	VALUE (7, 200);
+
+INSERT INTO arbeitsplatz
+	VALUE (8, 155);
+
+INSERT INTO arbeitsplatz
+	VALUE (9, 140);
+
+INSERT INTO arbeitsplatz
+	VALUE (10, 120);
+
+INSERT INTO arbeitsplatz
+	VALUE (11, 130);
+
+INSERT INTO arbeitsplatz
+	VALUE (12, 0);
+
+INSERT INTO arbeitsplatz
+	VALUE (13, 0);
+
+INSERT INTO arbeitsplatz
+	VALUE (14, 0);
+
+INSERT INTO arbeitsplatz
+	VALUE (15, 30);
+
