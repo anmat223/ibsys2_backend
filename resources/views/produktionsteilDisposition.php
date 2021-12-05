4b58cedb573858341e40b8558249564a0d8f2c78
Alicia Grüneberg <gral1026@hs-karlsruhe.de>
2021-12-05 14:44:07 +0100

    change tab set in prodTDisp, add bootstrapjs files

--- resources/views/produktionsteilDisposition.php
+++ resources/views/produktionsteilDisposition.php
@@ -5,19 +5,16 @@ require_once($documentRoot . '/ibsys2_backend/classes/services/Database_Service.
 $database = new DatabaseService();
 require_once($documentRoot . '/ibsys2_backend/navbar.php');
 ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<div class="container">
-  <h2>produktionsteildisposition</h2>
-  <ul class="nav nav-tabs">
-    <li class="active"><a data-toggle="tab" href="#p1">P1</a></li>
-    <li><a data-toggle="tab" href="#p2">P2</a></li>
-    <li><a data-toggle="tab" href="#p3">P3</a></li>
-  </ul>
-
-  <div class="tab-content">
-    <div id="p1" class="tab-pane fade in active">
+ <h2>produktionsteildisposition</h2>
+ <nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#p1" type="button" role="tab" aria-controls="nav-home" aria-selected="true">P1</button>
+    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#p2" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">P2</button>
+    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#p3" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">P3</button>
+  </div>
+</nav>
+  <div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active" id="p1" role="tabpanel" aria-labelledby="nav-home-tab">
       <h3>Disposition Eigenfertigung P1</h3>
       <div class="container">
         <table class="table table-bordered">
@@ -54,7 +51,7 @@ require_once($documentRoot . '/ibsys2_backend/navbar.php');
         </table>
       </div>
     </div>
-    <div id="p2" class="tab-pane fade">
+    <div id="p2" class="tab-pane fade" role="tabpanel" aria-labelledby="nav-profile-tab">
       <h3>Disposition Eigenfertigung P2</h3>
       <table class="table table-bordered">
         <thead>
@@ -89,7 +86,7 @@ require_once($documentRoot . '/ibsys2_backend/navbar.php');
         </tbody>
       </table>
     </div>
-    <div id="p3" class="tab-pane fade">
+    <div id="p3" class="tab-pane fade" role="tabpanel" aria-labelledby="nav-contact-tab">
       <h3>Disposition Eigenfertigung P3</h3>
       <table class="table table-bordered">
         <thead>
