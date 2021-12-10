d483be23be97b93117b08ea37acdbeda95373354
Vincent Mielke <Voelvimi@aol.com>
2021-12-10 10:10:19 +0100

    Unnötige Funktionen gelöscht

--- classes/services/Disposition_Kaufteile-Service.php
+++ classes/services/Disposition_Kaufteile-Service.php
@@ -5,39 +5,6 @@ require_once($documentRoot . '/ibsys2_backend/classes/entities/Periode.php');
 class DispositionKaufteileService
 {
 
-  function alleBestellungenBerechnen($kaufteile, $produktionsprogramm)
-  {
-    $bestellungen = array();
-
-    foreach ($kaufteile as $teil) {
-      $bestellung = $this->bestellungBerechnen($teil, $produktionsprogramm);
-      array_push($bestellungen, $bestellung);
-    }
-
-    return $bestellungen;
-  }
-
-  function bestellungBerechnen($kaufteil, $produktionsprogramm)
-  {
-    $bestellung = null;
-
-    $bruttobedarfPeriode = $kaufteil->p1 * $produktionsprogramm['p1'];
-    $bruttobedarfPeriode += $kaufteil->p2 * $produktionsprogramm['p2'];
-    $bruttobedarfPeriode += $kaufteil->p3 * $produktionsprogramm['p3'];
-
-    $endbestand = $kaufteil->anzahl - $bruttobedarfPeriode;
-
-    if ($endbestand <= $kaufteil->diskontmenge) {
-      if (($kaufteil->lieferzeit + $kaufteil->abweichung) > 1)
-        $bestellung = new Bestellung(new Periode(1), $kaufteil, true, $kaufteil->diskontmenge);
-      else {
-        $bestellung = new Bestellung(new Periode(1), $kaufteil, false, $kaufteil->diskontmenge);
-      }
-    }
-
-    return $bestellung;
-  }
-
   function berechnungBestellung($kaufteile)
   {
     $bestellungen = [];
