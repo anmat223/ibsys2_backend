147b2238e65133c1a14b33ce94d4ed749aefc19a
Vincent Mielke <Voelvimi@aol.com>
2021-12-11 18:41:52 +0100

    Footer hinzugefügt

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
+<footer class="bg-dark text-center text-white" style="margin-top: 25px;">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021 Alicia Grüneberg, Anne Matrusch, Niklas Uhr, Vincent Mielke
+  </div>
+</footer>
 </body>
 
 </html>
